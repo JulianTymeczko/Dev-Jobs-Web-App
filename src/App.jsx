138589c11a6b58d23477ee0697e040c30def4f37
Julian Houle-T <jhouletymeczko@gmail.com>
2023-09-14 19:53:36 -0400

    successfully integrated react into the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import '../src/style.css';
 import '../src/component.css';
-export default function App (props){
+import data from './data.json'
+
+export default function App (){
   return (
     <>
-   <div className="job-card">
-    <img src="./devjobs-web-app/starter-code/assets/logos/creative.svg" alt=""/>
+   
+    {data.map((el) => {
+      return (
+        <>
+        <div className="job-card" key={el.id}>
+        <img src={el.logo} alt="" style={{backgroundColor: el.logoBackground}}/>
         <div>
-            <h2>20h ago<span> . </span>Part Time</h2>
-            <h1>Haskell and PureScript Dev</h1>
-            <h2>Blogr</h2>
-            <h3>United States</h3>
+        <h2>{el.postedAt} <span> . </span>{el.contract}</h2>
+        <h1>{el.position}</h1>
+        <h2>{el.company}</h2>
+        <h3>{el.location}</h3>
         </div>
-    </div>
+        </div>
+        </>
+      )
+    })}
+  
     </>
   )
 }
+
--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,6 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App/>
-    <App/>
-    <App/>
-    <App/>
-    <App/>
-    <App/>
-    <App/>
-    <App/>
-    <App/>
     <a href="#" className="padded-anchor" id="load-more">Load More</a>
   </React.StrictMode>,
 )
