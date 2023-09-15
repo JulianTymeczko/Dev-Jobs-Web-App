76fc7e41dee378326e91502ca857c85cf59b8268
Julian Houle-T <jhouletymeczko@gmail.com>
2023-09-15 16:07:48 -0400

    fixed issue with first time rendering

--- src/script.js
+++ src/script.js
@@ -1,5 +1,20 @@
 export default function changePlaceholder (setPlaceholder) {
-window.addEventListener("resize", function () {
+  window.addEventListener("load", function () {
+    let windowWidth = window.innerWidth;
+  let y;
+
+ if (windowWidth <= 1439){
+   y = "Filter by title..."
+   setPlaceholder(y)
+  }
+  else if (windowWidth > 1440){
+   y = "Filter by title, companies, expertise..."
+   setPlaceholder(y)
+  }
+  })
+
+
+  window.addEventListener("resize", function () {
    let windowWidth = window.innerWidth;
    let x;
  
@@ -11,4 +26,6 @@ window.addEventListener("resize", function () {
     x = "Filter by title, companies, expertise..."
     setPlaceholder(x)
    }
-});}
+});
+
+}
