43a2e19edafffe59fbefe0a033106bf49f9ff3ed
Julian Houle-T <jhouletymeczko@gmail.com>
2023-09-15 22:38:08 -0400

    fixed issue involving job card layout

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
     changePlaceholder(setPlaceholder)
     return (
         <>
-            <div className="search-bar"><img src="/assets/desktop/icon-search.svg" alt="" /><input type="text" id="search-bar-input" placeholder={placeholder}/><div id="mobile-icons"><img src="/assets/mobile/icon-filter.svg" alt="" /><a href="#" className="padded-anchor"><img src="/assets/desktop/white-icon-search.svg" alt="" /></a></div></div>
+            <div className="search-bar"><img src="/assets/desktop/icon-search.svg" alt="" /><input type="text" id="search-bar-input" placeholder={placeholder}/><div id="mobile-icons"><img src="/assets/mobile/icon-filter.svg" alt="" id="mobile-filter"/><a href="#" className="padded-anchor"><img src="/assets/desktop/white-icon-search.svg" alt="" /></a></div></div>
             <div className="filter-location"><img src="/assets/desktop/icon-location.svg" alt="" /><input type="text" placeholder='Filter by location...' /></div>
             <div className="full-time-only"><input type="checkbox" id="myCheckbox" className="custom-checkbox" /><label htmlFor="myCheckbox" className="checkbox-label"></label><h3>Full Time <span>Only</span></h3><a href="#" className="padded-anchor">Search</a></div>
         </>
