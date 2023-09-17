8d695b09e442f1cb824e1bf06ecad0e6cb92fc84
Julian Houle-T <jhouletymeczko@gmail.com>
2023-09-16 22:21:21 -0400

    fixed issue with useeffect

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,37 +1,59 @@
 import "../src/job.css";
 import "../src/style.css";
 import "../src/component.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { changePlaceholder, firstTime } from "./script.js";
 export default function Nav() {
+  const [display, setDisplay] = useState("none");
   const [placeholder, setPlaceholder] = useState(firstTime());
+  useEffect(() => {
+    if (display == "inline-block") {
+      document.querySelector("html").setAttribute("style", "opacity: 0.4965;");
+    } else if (display == "none") {
+      document.querySelector("html").setAttribute("style", "opacity: 1");
+    }
+  }, [display]);
   changePlaceholder(setPlaceholder);
   return (
     <>
-      <div className="search-bar">
-        <img src="/assets/desktop/icon-search.svg" alt="" />
-        <input type="text" id="search-bar-input" placeholder={placeholder} />
-        <div id="mobile-icons">
-          <img src="/assets/mobile/icon-filter.svg" alt="" id="mobile-filter" />
+      <div style={{ display: display }}>dvfsdvds</div>
+      <>
+        <div className="search-bar">
+          <img src="/assets/desktop/icon-search.svg" alt="" />
+          <input type="text" id="search-bar-input" placeholder={placeholder} />
+          <div id="mobile-icons">
+            <img
+              src="/assets/mobile/icon-filter.svg"
+              alt=""
+              id="mobile-filter"
+              onClick={() => {
+                if (display == "none") {
+                  setDisplay("inline-block");
+                } else if (display == "inline-block") {
+                  setDisplay("none");
+                }
+              }}
+            />
+            <a href="#" className="padded-anchor">
+              <img src="/assets/desktop/white-icon-search.svg" alt="" />
+            </a>
+          </div>
+        </div>
+        <div className="filter-location">
+          <img src="/assets/desktop/icon-location.svg" alt="" />
+          <input type="text" placeholder="Filter by location..." />
+        </div>
+        <div className="full-time-only">
+          <input type="checkbox" id="myCheckbox" className="custom-checkbox" />
+          <label htmlFor="myCheckbox" className="checkbox-label"></label>
+          <h3>
+            Full Time <span>Only</span>
+          </h3>
           <a href="#" className="padded-anchor">
-            <img src="/assets/desktop/white-icon-search.svg" alt="" />
+            Search
           </a>
         </div>
-      </div>
-      <div className="filter-location">
-        <img src="/assets/desktop/icon-location.svg" alt="" />
-        <input type="text" placeholder="Filter by location..." />
-      </div>
-      <div className="full-time-only">
-        <input type="checkbox" id="myCheckbox" className="custom-checkbox" />
-        <label htmlFor="myCheckbox" className="checkbox-label"></label>
-        <h3>
-          Full Time <span>Only</span>
-        </h3>
-        <a href="#" className="padded-anchor">
-          Search
-        </a>
-      </div>
+      </>
     </>
   );
 }
